70bf87154ff379196932dcb7f06a719efd4c6737
grandmaster789 <mail@grandmaster.nu>
2019-01-20 22:10:54 +0100

    after much twiddling, got a triangle thingie working added postbuild script for compiling and copying shaders many issues remain, but it's a start

--- copyShaders.bat
+++ copyShaders.bat
@@ -1,6 +1,20 @@
-@rem %1 ~> platform target (x64, win32)
-@rem %2 ~> configuration (debug, release)
-@rem %3 ~> project root
-@rem %4 ~> target dir
+@echo off
+rem most of this is based on documentation from https://ss64.com/nt/for.html
 
-@xcopy /Y /D /I %3Djinn\Shaders %4\Shaders
+rem %1 ~> platform target (x64, win32)
+rem %2 ~> configuration (debug, release)
+rem %3 ~> solution dir
+rem %4 ~> target dir
+
+mkdir %4
+cd %3\djinn\shaders
+for %%d in (*.glsl) do call :compile %%d %4
+
+goto :end
+
+:compile
+echo glslangvalidator "%1" -V -o "%~2\%~n1.spv"
+glslangvalidator "%1" -V -o "%~2\%~n1.spv"
+exit /b
+
+:end
